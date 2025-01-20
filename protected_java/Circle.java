78a943a620b82da855016489cfb293d833d6607c
SUPRAVO BISWAS <draquyzastar1@gmail.com>
2025-01-20 10:33:04 +0530

    java lab

---
+++ protected_java/Circle.java
@@ -0,0 +1,18 @@
+package protected_java;
+
+public class Circle {
+    private double radius;
+    public void calculatePerimeter(double radius, double perimeter) {
+
+        perimeter=2*3.14*radius;
+    }
+
+    public void calculateArea(double radius) 
+    {
+    {
+        double area=3.14*radius*radius;
+        System.out.println("The area of the circle is: "+area);
+
+    }
+}
+}
